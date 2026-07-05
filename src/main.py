5d618d0d77f3ec771c33e0a0701fa1a73d139691
AcharaChisomSolomon <acharachisom861@gmail.com>
2026-07-05 18:09:54 +0100

    set up TextNode to represent inline elements

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode, TextType
+
+def main():
+    some_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    print(some_node)
+
+if __name__ == "__main__":
+    main()

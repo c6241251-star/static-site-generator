85450ed7ae78fba55649d6c2a262f5ec91c8efac
AcharaChisomSolomon <acharachisom861@gmail.com>
2026-07-10 20:07:53 +0100

    load from static to public

--- src/main.py
+++ src/main.py
@@ -1,10 +1,25 @@
-# from htmlnode import ParentNode, LeafNode
-# from textnode   import TextNode, TextType
-# from delimiting import split_nodes_delimiter, extract_markdown_images, extract_markdown_links, split_nodes_link, text_to_textnodes
+import os
+import shutil
 
-def main():
-    pass
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
-if __name__ == "__main__":
-    main()
+def main() -> None:
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+
+
+main()
